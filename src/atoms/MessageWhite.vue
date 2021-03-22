339c8267cc494c931d5eaa5306bf5b7890cc21e8
martaskarbek <48453376+martaskarbek@users.noreply.github.com>
2021-03-22 23:09:44 +0100

    added single message view

---
+++ src/atoms/MessageWhite.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="box">
+    I'm in a box. MBSxwbfex ajgrcwx eejhrct3we dcwerxu23ywf zdjyerx3fytwxrd sadxhxqfve zqADQCXRX23TQWC DQthxr3qth d
+    mAHSgdbjwae dsahrcgb ewa jhegrcwb  hfgweyfb  fjwehtfuvywfyw3t qhj3re23vx dqyg3rxy3w fwvyerxy3 w ryterwyer v
+    ahfdvewyta ahgsrfwe  hagfexy jaydfwevrtywrfyterfwyter ahetxy3uwr  hwtexb qher3x qw eh3xwf3r xvhwdfe
+    axwhegxbw3 ahwetcv ahuwd
+    <p>On BookShelf from 10.12.2020</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  }
+</script>
+
+<style>
+  .box {
+    color: #aaaaaa;
+  }
+  p {
+    font-size: 0.75rem;
+    margin-left: 82%;
+    margin-top: 2%;
+  }
+</style>
