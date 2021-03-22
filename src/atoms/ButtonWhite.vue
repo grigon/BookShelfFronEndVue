74567c2c0a5bbcccecfb0ec2f1a400b4ae37a5ac
martaskarbek <48453376+martaskarbek@users.noreply.github.com>
2021-03-23 00:32:13 +0100

    added button hover bgc color

---
+++ src/atoms/ButtonWhite.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="button is-danger is-outlined">Block this user</button>
+</template>
+
+<script>
+export default {
+  name: "ButtonWhite",
+}
+</script>
+
+<style scoped>
+.button-orange {
+  background-color: white;
+  color: #ea3918;
+  transition-duration: 0.3s;
+  link-hover-border: #ec6349;
+}
+.button-orange:hover {
+
+}
+</style>
