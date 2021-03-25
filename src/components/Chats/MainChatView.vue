c31c10694c46151d7907c722e14260c32a843f53
Dominik Wikliński <domel2222@gmail.com>
2021-03-25 12:58:21 +0100

    create main view chat component

---
+++ src/components/Chats/MainChatView.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <h1>MMMMMMMMMMMMMMMMMSDGGGGGGGGGGGGG</h1>
+        <div v-if="error" class="error">{{error}}</div>
+        <Suspense v-else>
+            <template #default>
+                <AllUserChats></AllUserChats>
+
+            </template>
+            <template #fallback>
+                <AppLoader />
+            </template>
+        </Suspense>
+    </div>
+</template>
+
+
+
+
+
+<script>
+import { onErrorCaptured, ref } from 'vue';
+export default {
+    components: {},
+
+    setup() {
+        const error = ref(null);
+
+        onErrorCaptured(e => {
+            error.value = e
+            return true;
+        });
+
+        return {error}
+    }
+}
+</script>
+
+<style lang="scss">
+.contaier {
+    width: 80vh;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #aa2222;
+    color: #000;
+}
+</style>
--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,22 +1,28 @@
 <template>
-  <MainNavView/>
+  <!-- <MainNavView/>
   <UserProfileForm/>
   Dupa
   Dupa
   Dupa
   Dupa
   Dupa
-  Dupa
+  Dupa -->
+  <MainChatView></MainChatView>
 </template>
 
 <script>
-import MainNavView from "./MainNavView";
-import UserProfileForm from "../components/UserProfileForm";
+// import MainNavView from "./MainNavView";
+// import UserProfileForm from "../components/UserProfileForm";
+import MainChatView from "../components/Chats/MainChatView";
+
+
+
 
 export default {
   components: {
-    UserProfileForm,
-    MainNavView
+    // UserProfileForm,
+    // MainNavView,
+    MainChatView
   },
 }
 </script>
